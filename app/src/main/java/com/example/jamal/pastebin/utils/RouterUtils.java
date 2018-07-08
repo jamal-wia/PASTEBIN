a7d11d61bb52868deed5c3e186ba87c66d3ec300
Aliev Jamal <aliev.djamal.2000@gmail.com>
2018-07-08 08:28:29 +0300

    add RouterUtils

--- app/src/main/java/com/example/jamal/pastebin/utils/RouterUtils.java
+++ app/src/main/java/com/example/jamal/pastebin/utils/RouterUtils.java
@@ -1,9 +1,11 @@
 package com.example.jamal.pastebin.utils;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 
-public class FragmentUtils {
+public class RouterUtils {
 
     public static void showFragmentInStack(int container, Fragment fragment,
                                            FragmentActivity fragmentActivity) {
@@ -12,8 +14,12 @@ public class FragmentUtils {
     }
 
     public static void showFragment(int container, Fragment fragment,
-                                           FragmentActivity fragmentActivity) {
+                                    FragmentActivity fragmentActivity) {
         fragmentActivity.getSupportFragmentManager().beginTransaction()
                 .replace(container, fragment).commit();
+    }
+
+    public void showActivity(Context context,Class<?> C){
+        context.startActivity(new Intent(context,C));
     }
 }
